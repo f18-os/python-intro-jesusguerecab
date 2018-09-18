0599a6fb84315dd018cb0c1208739cab146ef21e
jesusguerecab <jesusguerecab@gmail.com>
2018-09-17 20:21:54 -0600

    added shell.py permissions

--- shell/shell.py
+++ shell/shell.py
@@ -1,39 +1,31 @@
 #! /usr/bin/env python3
+#A shell's primary job is to ask a user what program to run next, and then to dispatch a new process to run it.
 
 import os, sys, time, re
 
+def execute(argv):
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+
+    elif rc == 0:
+        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
+                     (os.getpid(), pid)).encode())
+        #program = "%s%s" % (dir, args[0])
+        os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+        os.execve(program, args, os.environ)
+        os.write(2, ("Child: Could not exec %s\n" % args[0]).encode())
+        sys.exit(1)
+
+    else:                           # parent (forked ok)
+        childPidCode = os.wait()
+
+#----------MAIN----------
 pid = os.getpid()
 
-os.write(1, ("About to fork (pid:%d)\n" % pid).encode())
+input = input("$")
+args = input.split()
 
-rc = os.fork()
-
-if rc < 0:
-    os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
-
-elif rc == 0:                   # child
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                 (os.getpid(), pid)).encode())
-
-    #input = input("myshell:~" + os.getcwd()+">")
-    input = input("$")
-    args = input.split()
-    
-    for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-        program = "%s/%s" % (dir, args[0])
-        os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
-        try:
-            os.execve(program, args, os.environ) # try to exec program
-        except FileNotFoundError:             # ...expected
-            pass                              # ...fail quietly
-
-    os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
-    sys.exit(1)                 # terminate with error
-
-else:                           # parent (forked ok)
-    os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
-                 (pid, rc)).encode())
-    childPidCode = os.wait()
-    os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
-                 childPidCode).encode())
+execute(args)

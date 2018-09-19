4f8e5481d6111fa567d6f6a0564577f8e0f6a37c
jesusguerecab <jesusguerecab@gmail.com>
2018-09-19 16:40:23 -0600

    fork is now done after input is procesed; fork and execute combined

--- shell/shell.py
+++ shell/shell.py
@@ -5,6 +5,7 @@ import os, sys, time, re
 
 def parse(uinput):
     args = uinput.split()
+    #Checks for environmental variables $
     for arg in args:
         if '$' in arg:
             arg = arg.replace("$","")
@@ -13,6 +14,8 @@ def parse(uinput):
             except KeyError:
                 os.write(2, ("Key Error: %s" % arg).encode())
     args = uinput.split()
+
+    #Handles output redirect
     if '>' in uinput:
         os.close(1)
         sys.stdout = open(args[args.index('>')+1],'w')
@@ -20,32 +23,23 @@ def parse(uinput):
         os.set_inheritable(fd,True)
               
         execute(args[:args.index('>')])
+
+    #Handles changing environmental variables
     elif '=' in uinput:
         uinput = uinput.replace(" ","")
         args = uinput.split("=")
         os.environ[args[0]] = args[1]
         #print(os.environ['a'])
         sys.exit(1)
+
+    #Handles directory change
     elif 'cd' in uinput:
         os.chdir(args[1])
     else:
         execute(args)
 
+       
 def execute(args):
-    #try to execute with given directory
-    try:
-        os.execve(args[0],args,os.environ)
-    except FileNotFoundError:
-        pass
-    #try to execute with directories in PATH
-    for dir in re.split(":", os.environ['PATH']):
-        program = "%s/%s" % (dir, args[0])
-        try:
-            os.execve(program, args, os.environ)
-        except FileNotFoundError:  
-            pass
-        
-def fork(uinput):
     rc = os.fork()
 
     if rc < 0:
@@ -53,7 +47,18 @@ def fork(uinput):
         sys.exit(1)
 
     elif rc == 0:
-        parse(uinput)
+        #try to execute with given directory
+        try:
+            os.execve(args[0],args,os.environ)
+        except FileNotFoundError:
+            pass
+        #try to execute with directories in PATH
+        for dir in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dir, args[0])
+            try:
+                os.execve(program, args, os.environ)
+            except FileNotFoundError:  
+                pass
         os.write(2, ("Child: Could not exec %s\n" % uinput.split()[0]).encode())
         sys.exit(1)
 
@@ -68,6 +73,5 @@ user_input = input("$")
 os.environ['a'] = "2"
 
 while 'exit' not in user_input:
-    fork(user_input)
-
+    parse(user_input)
     user_input = input("$")

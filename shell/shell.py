1133601874e5bf2872799f123ff50627b5d65008
jesusguerecab <jesusguerecab@gmail.com>
2018-09-19 22:26:37 -0600

    fixed errors caused by adding pipe code

--- shell/shell.py
+++ shell/shell.py
@@ -32,48 +32,14 @@ def parse(uinput):
         uinput = uinput.replace('<','')
         args = uinput.split()
         execute(args)
-    #Handles changing environmental variables
-    elif '=' in uinput:
-        uinput = uinput.replace(" ","")
-        args = uinput.split("=")
-        os.environ[args[0]] = args[1]
-        #print(os.environ['a'])
-        return
-
-    #Handles directory change
-    elif 'cd' in uinput:
-        os.chdir(args[1])
-
     #Handle pipes
     elif '|' in uinput:
-        pr,pw = os.pipe()
-        for f in (pr, pw):
-            os.set_inheritable(f, True)
-            
-        _rc = os.fork()
-
-        if _rc < 0:
-            sys.exit(1)
-
-        elif _rc == 0:
-            args = args[:args.index['|']]
-            os.close(1)
-            os.dup(pw)
-            for fd in (pr,pw):
-                os.close(fd)
-            execute(args)
-        else:
-            args = args[args.index['|']+1:]
-            os.close(0)
-            os.dup(pr)
-            for fd in (pw, pr):
-                os.close(fd)
-            execute(args)
+        pass
     #Default
     else:
         execute(args)
-
-       
+                        
+                        
 def execute(args):
         #try to execute with given directory
         try:
@@ -93,19 +59,31 @@ def execute(args):
 #----------MAIN----------
     
 user_input = ""
-    
+
 while 'exit' not in user_input:
     try:
         user_input = input(os.environ['PS1'])
     except KeyError:
         user_input = input('$ ')
-
-    rc = os.fork()
-
-    if rc < 0:
-        os.write(2, ("fork failed, returning %d\n" % rc).encode())
-        sys.exit(1)
-    elif rc == 0:
-        parse(user_input)
+    
+    #Handles directory change
+    if 'cd' in user_input:
+        args = user_input.split()
+        os.chdir(args[1])
+    #Handles changing environmental variables
+    elif '=' in user_input:
+        uinput = user_input.replace(" ","")
+        args = user_input.split("=")
+        os.environ[args[0]] = args[1].replace("\"","")
+        #print(os.environ[arg[0]])
     else:
-        childPidCode = os.wait()
+        rc = os.fork()
+
+        if rc < 0:
+            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            sys.exit(1)
+        elif rc == 0:
+            parse(user_input)
+            sys.exit(1)
+        else:
+            childPidCode = os.wait()
